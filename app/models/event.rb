77e4f0b23ebd7ce1d9105c1f8c1e2cedb706068a
Thomas Simpson <thomas.simpson768@gmail.com>
2022-01-16 23:22:07 +0000

    Update models to allow for attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :host, class_name: 'User'
+  has_many :event_attendees, foreign_key: :event_id
+  has_many :atendees, through: :event_attendees, source: :attendee
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :hosted_events, foreign_key: 'host_id', class_name: 'Event'
+  has_many :event_attendees, foreign_key: :attendee_id
+  has_many :attended_events, through: :event_attendees, source: :events
 end
