9c1a3205126605776cfc1555b93b70a51ee2dd09
Thomas Simpson <thomas.simpson768@gmail.com>
2022-01-19 17:23:56 +0000

    Ensure event attendees saves to db

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -1,7 +1,7 @@
-class EventAttendeeController < ApplicationController
+class EventAttendeesController < ApplicationController
   def create
-    @event.find(params[:id])
-    @event.event_attendees.build(attendee_id: current_user.id)
+    @event = Event.find(params[:event_id])
+    @event.event_attendees.build(attendee_id: current_user.id).save
     redirect_to event_path(@event)
   end
 end
