1d61c62cc07409438c0409ffc85745073356efe1
Thomas Simpson <thomas.simpson768@gmail.com>
2022-01-19 17:07:41 +0000

    Create event_attendees partial

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -1,7 +1,7 @@
 class EventAttendeeController < ApplicationController
   def create
     @event.find(params[:id])
-    current_user.event_attendees.build(event_id: @event.id)
+    @event.event_attendees.build(attendee_id: current_user.id)
     redirect_to event_path(@event)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'events#index'
-  resources :events
+  resources :events do
+    resources :event_attendees
+  end
   get 'user/:id', to: 'user#show'
 end
