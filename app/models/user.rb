6771aa6cc07e41102750772b9c101be913652f47
Thomas Simpson <thomas.simpson768@gmail.com>
2022-01-19 19:16:05 +0000

    Seperate future and past events on user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   
   has_many :hosted_events, foreign_key: 'host_id', class_name: 'Event'
   has_many :event_attendees, foreign_key: :attendee_id
-  has_many :attended_events, through: :event_attendees, source: :events
+  has_many :attended_events, through: :event_attendees, source: :event
 end
