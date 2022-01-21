b6df3fa9d83217b3dd1563b148cdde9f8f792607
Thomas Simpson <thomas.simpson768@gmail.com>
2022-01-21 21:39:24 +0000

    Add a destroy method to eventattendees controller

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -4,4 +4,9 @@ class EventAttendeesController < ApplicationController
     @event.event_attendees.build(attendee_id: current_user.id).save
     redirect_to event_path(@event)
   end
+
+  def destroy
+    @event_attendee = EventAttendee.where('event_id = ? and attendee_id = ?', params[:event_id], current_user.id)
+    @event_attendee.destroy
+  end
 end
