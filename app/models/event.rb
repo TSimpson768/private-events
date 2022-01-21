6cfdb9c414d0f5474560a4209143054839930e37
Thomas Simpson <thomas.simpson768@gmail.com>
2022-01-21 20:53:38 +0000

    Refactor past + future into scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,14 @@ class Event < ApplicationRecord
   has_many :event_attendees, foreign_key: :event_id
   has_many :attendees, through: :event_attendees, source: :attendee
 
-  def self.past
-    Event.where('start_time <= ?', DateTime.now)
-  end
+  scope :past, -> { where('start_time <= ?', DateTime.now) }
+  scope :future, -> { where('start_time >= ?', DateTime.now) }
 
-  def self.future
-    Event.where('start_time >= ?', DateTime.now)
-  end
+  # def self.past
+  #   Event.where('start_time <= ?', DateTime.now)
+  # end
+
+  # def self.future
+  #   Event.where('start_time >= ?', DateTime.now)
+  # end
 end
